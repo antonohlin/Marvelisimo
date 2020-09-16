111b24e3fe0158b4be86997d01cccc884a5fdd92
Anton <anton.ohlin@hotmail.com>
2020-09-16 11:10:25 +0200

    removed unused imports

--- app/src/main/java/com/example/marvelisimo/MarvelRetrofit.kt
+++ app/src/main/java/com/example/marvelisimo/MarvelRetrofit.kt
@@ -1,8 +1,5 @@
 package com.example.marvelisimo
 
-import android.content.Context
-import com.squareup.picasso.OkHttp3Downloader
-import com.squareup.picasso.Picasso
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
