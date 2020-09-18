aadaae31e3d02098b0e9d9dfec7ff792a8e893d1
michelle opey <mipanowicz@gmail.com>
2020-09-18 11:04:04 +0200

    Navbar + search icon

--- app/src/main/java/com/example/marvelisimo/fragment/MainActivity.kt
+++ app/src/main/java/com/example/marvelisimo/fragment/MainActivity.kt
@@ -4,8 +4,11 @@ import android.content.Intent
 import android.icu.util.TimeUnit
 import android.os.Bundle
 import android.os.Handler
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.marvelisimo.R
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,8 +18,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         start_button.setOnClickListener { v -> fade() }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.layout_navbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var itemview = item.itemId
+        when (itemview) {
+            R.id.search_bar -> Toast.makeText(this, "Search clicked!", Toast.LENGTH_SHORT)
+        }
+        return false
     }
 
     private fun fade() {
